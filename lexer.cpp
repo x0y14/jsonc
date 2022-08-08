a0770f6f3f3bf1a932a67c74f9fae0bb7c553b32
x0y14 <ya01ma08@gmail.com>
2022-08-08 15:54:37 +0900

    clean up

--- lexer.cpp
+++ lexer.cpp
@@ -10,13 +10,12 @@ namespace jsonc {
 
 vector<char> json;
 int pos;
-
 vector<jsonc::Token> tokens;
 
 
 char peek() {
     return json[pos+1];
-}
+};
 
 char curt() {
     return json[pos];
@@ -30,150 +29,52 @@ bool is_eof() {
     return pos >= json.size();
 }
 
-// 現在のキャラクタが空白、改行である間削除し続けます
-void t_ws() {
-    jsonc::Token token;
-    token.kind = TWHITE;
-    token.s_pos = pos;
-    char c;
+void t_json() {}
 
-    while (!is_eof()) {
-        c = curt();
-        if( c == ' ' || c == '\n' ) {
-            token.data += string({c});
-            go_next();
-        } else {
-            break;
-        }
-    }
+void t_value() {}
 
-    token.e_pos = pos;
-    if (token.data == "") {
-        return;
-    }
+void t_object() {}
 
-    tokens.push_back(token);
-}
+void t_members() {}
 
-void t_member() {
-    t_ws();
-    t_string();
-    t_ws();
-
-    //
-    jsonc::Token colon = {.s_pos=pos, .e_pos=pos+1, .data=":", .kind=TCOLON};
-    tokens.push_back(colon);
-    go_next();
-
-    t_element();
-}
-
-void t_members() {
-    t_member();
-    if (curt() == ',') {
-        jsonc::Token commma = {.s_pos=pos, .e_pos=pos+1, .data=",", .kind=TCOMMMA};
-        tokens.push_back(commma);
-        go_next();  
-        t_members();
-    }
-}
-
-void t_object() {
-    // cout << "t_object" << endl;
-
-    if (curt() != '{'){
-        throw std::invalid_argument( "expected: {, but found: " + string{curt()} ); }
-    
-    jsonc::Token t_lcub;
-    t_lcub.s_pos = pos;
-    t_lcub.kind = TLCUB;
-    t_lcub.data += string({curt()});
-    go_next();
-    t_lcub.e_pos = pos;
-    tokens.push_back(t_lcub);
-    
-    t_ws();
-
-    if (curt() != '}') {
-        t_members();
-    }
-
-    jsonc::Token t_rcub;
-    t_rcub.s_pos = pos;
-    t_rcub.kind = TRCUB;
-    t_rcub.data += string({curt()});
-    go_next();
-    t_rcub.e_pos = pos;
-    tokens.push_back(t_rcub);
-}
-
+void t_member() {}
 
 void t_array() {}
+
+void t_elements() {}
+
+void t_element() {}
+
 void t_string() {}
+
+void t_characters() {}
+
+void t_character() {}
+
+void t_escape() {}
+
+void t_hex() {}
+
 void t_number() {}
-void t_true() {}
-void t_false() {}
-void t_null() {}
 
-void t_value() {
-    // cout << "t_value" << endl;
+void t_integer() {}
 
-    if (curt() == '{') {
-        t_object();
-    } else if (curt() == '[') {
-        t_array();
-    } else if (curt() == '"') {
-        t_string();
-    } else if (curt() == 't') {
-        t_true();
-    } else if (curt() == 'f') {
-        t_false();
-    } else if (curt() == 'n') {
-        t_null();
-    } else if (string("-0123456789").find(curt()) != string::npos) {
-        t_number();
-    } else {
-        string msg = "unexpected char: " + string{curt()};
-        throw std::invalid_argument( msg );
-    }
-}
+void t_digits() {}
 
+void t_digit() {}
 
-void t_element() {
-    // cout << "t_element" << endl;
+void t_onenine() {}
 
-    t_ws();
-    if (is_eof()) {
-        return;
-    }
-    t_value();
-     if (is_eof()) {
-        return;
-    }
-    t_ws();
-}
+void t_fraction() {}
 
-void t_json() {
-    t_element();
-}
+void t_exponent() {}
 
+void t_sign() {}
+
+void t_ws() {}
 
 vector<jsonc::Token> lex(string text) {
-    vector<char> v(text.begin(), text.end());
-    json = v;
-    pos = 0;
-    tokens = {};
-
-    // while ( !is_eof() ) {
-    //     // cout << curt() << endl;
-    //     t_json();
-    //     go_next();
-    // }
-
-    t_json();
-
-    return tokens;
+    return {};
 }
-
 
 } // namespace jsonc
